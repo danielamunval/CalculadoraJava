dc17bc90e0dbec66e7aa338236a8f44f29202ba7
Daniela Muñoz Valdés <daniela.constanza03@gmail.com>
2025-06-18 21:27:59 -0400

    actualización

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -2,37 +2,63 @@ import java.util.Scanner;
 
 public class Calculadora {
     public static void main(String[] args) {
+        //Inicio variables en 0 e instancia de Scanner.
         Scanner teclado = new Scanner(System.in);
-        int numeroUno = 0;
-        int numeroDos = 0;
+        float numeroUno = 0;
+        float numeroDos = 0;
         int opcion = 0;
+        char continuacion = 's';
 
-        System.out.println("Hola y bienvenido/a!" + "\n");
-        System.out.println("Por favor, indica qué operación matemática necesitas hacer:" + "\n");
-        System.out.println("Indica 1 para sumar, 2 para restar, 3 para multiplicar, 4 para dividir o 5 para módulo: ");
+        //Mensaje bienvenida
+        System.out.println("*** Programa calculadora ***");
+        System.out.println("Hola y bienvenido/a!");
 
-        opcion = teclado.nextInt();
+        while (continuacion == 's' || continuacion == 'S') {
 
-        System.out.println("Ingresa el primer número:");
-        numeroUno = teclado.nextInt();
-        System.out.println("Ingresa el segundo número:");
-        numeroDos = teclado.nextInt();
+            //Selección de operación artimética.
+            System.out.println("Por favor, indica qué operación matemática necesitas hacer:");
+            System.out.println("Indica 1 para sumar, 2 para restar, 3 para multiplicar, 4 para dividir o 5 para módulo: ");
 
-        if (opcion == 1) {
-            System.out.println("El resultado de la SUMA es = " + (numeroUno + numeroDos));
+            //Se asigna variable opción según lo ingresado en el teclado
+            opcion = teclado.nextInt();
+
+            //Bucle en caso de que no se registre una opción válida.
+            while (opcion != 1 && opcion != 2 && opcion != 3 && opcion != 4 && opcion != 5) {
+                System.out.println("El número indicado no corresponde, favor reintenta agregar tu opción" + "\n");
+                System.out.println("Por favor, indica qué operación matemática necesitas hacer:");
+                System.out.println("Indica 1 para sumar, 2 para restar, 3 para multiplicar, 4 para dividir o 5 para módulo: ");
+                opcion = teclado.nextInt();
+            }
+
+            //Se asignan números según lo ingresado en el teclado
+            System.out.println("Ingresa el primer número:");
+            numeroUno = teclado.nextInt();
+            System.out.println("Ingresa el segundo número:");
+            numeroDos = teclado.nextInt();
+
+            //Según la operación solicitada, se ejecuta la condición que corresponda.
+            if (opcion == 1) {
+                System.out.println("El resultado de la SUMA es = " + (numeroUno + numeroDos));
+            }
+            if (opcion == 2) {
+                System.out.println("El resultado de la RESTA es = " + (numeroUno - numeroDos));
+            }
+            if (opcion == 3) {
+                System.out.println("El resultado de la MULTIPLICACIÓN es = " + (numeroUno * numeroDos));
+            }
+            if (opcion == 4) {
+                System.out.println("El resultado de la DIVISIÓN es = " + (numeroUno / numeroDos));
+            }
+            if (opcion == 5) {
+                System.out.println("El resultado del MÓDULO es = " + (numeroUno % numeroDos));
+            }
+
+            System.out.println("\n" + "¿Quieres hacer otra operacion?");
+            System.out.println("Ingresa si o no");
+            continuacion = teclado.next().charAt(0);
+
         }
-        if (opcion == 2) {
-            System.out.println("El resultado de la RESTA es = " + (numeroUno - numeroDos));
-        }
-        if (opcion == 3) {
-            System.out.println("El resultado de la MULTIPLICACIÓN es = " + (numeroUno * numeroDos));
-        }
-        if (opcion == 4) {
-            System.out.println("El resultado de la DIVISIÓN es = " + (numeroUno / numeroDos));
-        }
-        if (opcion == 5) {
-            System.out.println("El resultado del MÓDULO es = " + (numeroUno % numeroDos));
-        }
+        System.out.println("¡Gracias por usar esta calculadora!");
 
     }
 
